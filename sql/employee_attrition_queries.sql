48b0c09505796459df4b811b108b15f375240219
Samantha Freeland <samantha.freeland@ucdenver.edu>
2026-07-23 12:17:18 -0600

    Query 2
    Which departments have the highest turnover?

--- sql/employee_attrition_queries.sql
+++ sql/employee_attrition_queries.sql
@@ -1,3 +1,5 @@
+-- Query 1: 
+-- Question: What is the overall employee attrition rate? 
 SELECT
 	attrition, 
 	COUNT(*) AS employees, 
@@ -8,3 +10,18 @@ SELECT
 FROM hr_attrition
 GROUP BY attrition
 ORDER BY attrition;
+
+-- Query 2: 
+-- Question: Which departments have the highest turnover? 
+
+SELECT
+	department,
+	COUNT(*) AS total_employees, 
+	SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) AS employees_left,
+	ROUND(
+		SUM(CASE WHEN attrition = 'Yes' THEN 1 ELSE 0 END) * 100.0 / COUNT(*),
+		2
+		) AS attrition_rate
+		FROM hr_attrition
+		GROUP BY department
+		Order BY attrition_rate DESC;
